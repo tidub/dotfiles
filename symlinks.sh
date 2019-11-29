f64bf22174ece5efc2125b375de1af5b15d7941a
tidub <57918941+tidub@users.noreply.github.com>
2019-11-29 00:41:23 +0000

    improved comments

--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# This script backs up old dotfiles and symlinks to latest dotfiles
+# This script:
+# Backs up old dotfiles in ~/dotfiles_old
+# Symlinks last pulled ~/dotfiles to ~ or ~/.config/...
 
 # Files in ~ assigned to variable $files
 # Files not in ~ handled as separate cases
 
+# Variables:
 dir=~/dotfiles
 old_dir=~/dotfiles_old
 files="bash_profile vimrc zshrc"
@@ -29,11 +32,13 @@ done
 
 # Move config files in ~/.config to dotfiles_old, then create symlinks
 
+# ~/.config/karabiner/karabiner.json
 echo "Moving config file karabiner.json to $old_dir"
 mv ~/.config/karabiner/karabiner.json $old_dir/karabiner.json
 echo "Creating symlink to karabiner.json in ~/.config/karabiner"
 ln -s $dir/karabiner.json ~./config/karabiner/karabiner.json
 
+# ~/.config/nvim.init.vim
 echo "Moving config file init.vim to $old_dir"
 mv ~/.config/nvim/init.vim $old_dir/init.vim
 echo "Creating symlink to init.vim in ~/.config/nvim"
