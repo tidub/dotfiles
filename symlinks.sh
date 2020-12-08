dd3bad3653d7b1158c25f24766c92fbba7d1c2c5
tidub <57918941+tidub@users.noreply.github.com>
2020-12-08 15:57:17 +0000

    improved readability of script

--- symlinks.sh
+++ symlinks.sh
@@ -3,28 +3,28 @@
 # Keep your dotfiles and config files up-to-date across machines.
 
 # This script:
-# Creates directories clone_dir and backup_dir
-# Local dotfiles and config files are backed up in backup_dir
-# Remote dotfiles and config files are cloned into clone_dir
+# Creates directories main_directory and backup_directory
+# Local dotfiles and config files are backed up in backup_directory
+# Remote dotfiles and config files are cloned into main_directory
 # Local dotfiles and config files are replaced with
-# symlinks to their up-to-date namesakes in clone_dir
+# symlinks to their up-to-date namesakes in main_directory
 
-# Directories created
-clone_dir=~/dotfiles
-backup_dir=~/dotfiles_old
+# Directories created / updated
+main_directory=~/dotfiles
+backup_directory=~/dotfiles_old
 
 # Git remote url
-git_url="https://github.com/tidub/dotfiles.git"
+github_repository="https://github.com/timdubbins/dotfiles.git"
 
 # Dotfiles ~/."..."
-files="
+dotfiles="
 	bash_profile
 	vimrc
 	zshrc
 	tmux.conf
 "
 
-# Config files ~/.config/"..."
+# Configuration files ~/.config/"..."
 config_files=(
 	"karabiner/karabiner.json"
 	"nvim/init.vim"
@@ -33,7 +33,7 @@ config_files=(
 # Tell user what the script does
 echo "This script will update local files with their remote namesakes at..."
 echo
-echo "$git_url"
+echo "$github_repository"
 echo
 
 # User prompt
@@ -44,35 +44,35 @@ then
 
 	# Setup directory to clone into
 	echo
-	echo "Check $clone_dir exists and make it the current directory..."
-	mkdir $clone_dir
-	cd $clone_dir
+	echo "Check $main_directory exists and make it the current directory..."
+	mkdir $main_directory
+	cd $main_directory
 	echo "...done!"
 	echo
 
 	# Setup git remote
-	echo "Cloning from $git_url into $clone_dir..."
+	echo "Clone $github_repository to $main_directory..."
 	git init
-	git remote add origin "$git_url"
+	git remote add origin "$github_repository"
 	git pull origin master
 	echo "...done!"
 	echo
 
 	# Setup backup and config directories
-	echo "Emptying $backup_dir..."
-	rm -rf $backup_dir
-	mkdir -p $backup_dir
+	echo "Emptying $backup_directory..."
+	rm -rf $backup_directory
+	mkdir -p $backup_directory
 	echo "Checking ~/.config exists..."
 	mkdir ~/.config
 	echo "...done!"
 	echo
 
 	# Backup dotfiles and create symlinks
-	for file in $files; do
-		echo "Sending $file copy to $backup_dir..."
-		mv ~/.$file $backup_dir
+	for file in $dotfiles; do
+		echo "Sending $file copy to $backup_directory..."
+		mv ~/.$file $backup_directory
 		echo "Creating symlink..."
-		ln -s $clone_dir/$file ~/.$file
+		ln -s $main_directory/$file ~/.$file
 		echo "...$file updated successfully!"
 		echo
 	done
@@ -81,12 +81,12 @@ then
 	for i in "${config_files[@]}"; do
 		config_dir="${i%%/*}"
 		config_file="${i##*/}"
-		echo "Sending $config_file copy to $backup_dir..."
-		mv ~/.config/$config_dir/$config_file $backup_dir/$config_file
+		echo "Sending $config_file copy to $backup_directory..."
+		mv ~/.config/$config_dir/$config_file $backup_directory/$config_file
 		echo "Checking directory for symlink exists..."
 		mkdir ~/.config/$config_dir
 		echo "Creating symlink..."
-		ln -s $clone_dir/$config_file ~/.config/$config_dir/$config_file
+		ln -s $main_directory/$config_file ~/.config/$config_dir/$config_file
 		echo "...$config_file updated successfully!"
 		echo
 	done
